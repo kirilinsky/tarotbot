1de6dbcf29e26fd81af2bff37854f5696016ab97
kirilinsky <kirill.ilinsky@gmail.com>
2025-06-23 10:19:26 +0200

    add logs

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,12 @@ dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN!);
 
-bot.start((ctx) => ctx.reply("hello there 🔮"));
+bot.start((ctx) => ctx.reply("hello from railways 🔮"));
+bot.command("ping", (ctx) => ctx.reply("pong"));
+
 bot.launch();
 
 console.log("Bot is running...");
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
