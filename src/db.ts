191a3937f28b578d0ca0303dac6952e1c03df708
kirilinsky <kirill.ilinsky@gmail.com>
2026-04-13 23:30:57 +0200

    fix: update db connection

--- src/db.ts
+++ src/db.ts
@@ -4,10 +4,10 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-const rawUrl = process.env.DATABASE_URL ?? "";
-const dbUrl = rawUrl.includes("${{")
-  ? process.env.DATABASE_PUBLIC_URL!
-  : rawUrl;
+const dbUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL!
+    : process.env.DATABASE_PUBLIC_URL!;
 
 export const sql = postgres(dbUrl, {
   ssl: "require",
